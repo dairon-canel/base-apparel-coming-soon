749807517f2b55e465cf05a6de772cd5632e8e2e
Dairon Canel <98697567+RyuzakCoder@users.noreply.github.com>
2022-12-06 19:38:43 -0500

    feat: error states logic

--- public/index.js
+++ public/index.js
@@ -1,13 +1,54 @@
-let toggled = false;
-const toggledOnClassName = document.getElementById('share-button').className;
-const toggledOffClassName = toggledOnClassName.replace('hidden', 'grid');
+const form = document.querySelector('form');
+const email = document.getElementById('mail');
+const error = email.nextElementSibling;
+const errorIcon = error.nextElementSibling;
 
-const toggle = () => {
-  if (!toggled) {
-    document.getElementById('share-button').className = toggledOffClassName;
-    toggled = true;
+const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const setError = errorString => {
+  email.classList.add('invalid-form');
+  error.textContent = errorString;
+  error.classList.remove('hidden');
+  errorIcon.classList.remove('hidden');
+};
+
+const setValidate = () => {
+  email.classList.remove('invalid-form');
+  error.textContent = '';
+  error.classList.add('hidden');
+  errorIcon.classList.add('hidden');
+};
+
+const validate = () => {
+  setValidate();
+  window.alert(email.value);
+  email.value = '';
+};
+
+email.addEventListener('focus', event => {
+  setValidate();
+});
+
+email.addEventListener('blur', event => {
+  setValidate();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  let errorMessage = '';
+
+  if (email.value.length === 0) {
+    errorMessage = 'Whoops! It looks like you forgot to add your email';
+    setError(errorMessage);
     return;
   }
-  document.getElementById('share-button').className = toggledOnClassName;
-  toggled = false;
-};
+
+  if (!emailRegExp.test(email.value)) {
+    errorMessage = 'Please provide a valid email';
+    setError(errorMessage);
+    return;
+  }
+
+  validate();
+});
